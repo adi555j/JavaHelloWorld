8e44b333fc51725d86a19d956146084bda8d3b51
adi555j <adi555j@gmail.com>
2023-08-16 17:12:47 +0530

    task.wait

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,23 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+	wait(5000);
+	System.out.println("Hello world again!");
+	wait(50000);
+	System.out.println("Hello world again and again!");
+	wait(5000000);
+	System.out.println("Hello world again and again after 500000ms!");
+	
+  }
+  public static void wait(int ms)
+  {
+	  try
+	  {
+		  Thread.sleep(ms);
+	  }
+	  catch(InterruptedException ex)
+	  {
+		  Thread.currentThread().interrupt();
+	  }
   }
 }
